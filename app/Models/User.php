7619a3026637f0f46610d3bbb387a9be1083e3f3
wilmy <wilmyrb@gmail.com>
2022-05-17 12:48:44 -0400

    Laravel Roles usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isAdmin(){
+        return $this->rol == "admin";
+    }
+
+    public function isRegular(){
+        return $this->rol == "regular";
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('rol', ["admin", "subAdmin", "regular","basic"])->default('regular');
             $table->rememberToken();
             $table->timestamps();
         });
